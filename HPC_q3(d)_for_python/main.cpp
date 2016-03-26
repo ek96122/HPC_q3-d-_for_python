4516f6af08f83458f1b4cf08c2dbfcfac2da4ac5
Ernest Kan <ek96122@gmail.com>
2016-03-26 16:23:22 +0000

    Initial Commit

---
+++ HPC_q3(d)_for_python/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HPC_q3(d)_for_python
+//
+//  Created by Ernest on 26/3/2016.
+//  Copyright © 2016 Kan Tsz Hang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
